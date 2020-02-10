1ffb7506c2ea9d29cf0c4b96d3bd2c0a15ad6014
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-10 17:46:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,12 +7,12 @@ def directors_totals(nds)
     director_row = 0
     while director_row < nds.count do
         movies = 0
-        gross_inner = 0
+        gross_sales = 0
         while movies < nds[director_row][:movies].count do
-            gross_inner += nds[director_row][:movies][movies][:worldwide_gross]
+            gross_sales += nds[director_row][:movies][movies][:worldwide_gross]
             movies += 1
         end
-        result[nds[director_row][:name]] = gross_inner
+        result[nds[director_row][:name]] = gross_sales
         director_row += 1
     end
     result
