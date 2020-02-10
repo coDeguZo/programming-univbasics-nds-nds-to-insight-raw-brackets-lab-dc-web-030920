4ffca8449e482fd29377e322d9878c23019a544a
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-10 17:38:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,18 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp directors_database
+    nds = directors_database
+    result = {}
+    director_row = 0
+    while director_row < nds.count do
+        movies = 0
+        gross_inner = 0
+        while movies < nds[director_row][:movies].count do
+            gross_inner += nds[director_row][:movies][:worldwide]
+            movies += 1
+        end
+        gross_inner += 1
+    end
+    result[nds[director_row][:movies]] = gross_inner
+    director_row += 1
 end
-
-directors_totals(directors_database)
